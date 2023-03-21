fa1adfd0f08e0f440a58820f1f3e00c0e8520c46
dmitrybv <dimabol@yandex.ru>
2023-03-21 17:19:21 +0300

    Vue component 'sh' instead of custom HTML Tag 'sh'

---
+++ src/.vuepress/components/sh.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sh-class">
+    <slot></slot> <!-- slot outlet -->
+  </div>
+</template>
+
+<script>
+  export default {
+    // name: 'sh'
+  }
+</script>
+
+<style scoped>
+  .sh-class {
+    display: block;
+    margin-inline-start: 40px;
+  }
+</style>
